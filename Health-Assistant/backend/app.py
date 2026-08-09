28d4f305f36722bc4316bb85a7e255a66f4335c1
Yash <163550213+yash-data-tools@users.noreply.github.com>
2026-08-09 23:45:17 +0530

    Update app.py

--- Health-Assistant/backend/app.py
+++ Health-Assistant/backend/app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 
 app.register_blueprint(assistant_bp)
 app.register_blueprint(bmi_bp)
-CORS(app, origins=["https://health-assistant-vercel.vercel.app/"])
+CORS(app, resources={
+  r"/api/*":{
+    "origins":["https://health-assistant-vercel.vercel.app/"]
+  }
+})
 
 if __name__ == "__main__":
   app.run(debug=True)
